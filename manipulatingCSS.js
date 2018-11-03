e1079910ee8ed39a026cbf049bed63086049f494
nimax <nimax29@gmail.com>
2018-11-03 18:44:12 -0500

    CSS

---
+++ manipulatingCSS.js
@@ -0,0 +1,18 @@
+
+document.getElementById("body").style.background = "#114B5F";
+document.getElementById("body").style.color = "#fff";
+document.getElementById("body").style.fontFamily = "Georgia";
+
+
+
+
+var tit= document.getElementsByClassName("title");
+tit[0].style.color = "#F0C808";
+
+var li= document.getElementsByClassName("list");
+var a=document.getElementsByClassName("link");
+    for(var i=0; i<=li.length;i++){
+            li[i].style.color = "#F4AD46";
+            a[i].style.color = "#F4AD46";
+            a[i].style.color = "#F4AD46";
+        }
